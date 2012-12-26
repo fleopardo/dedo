9e513756471127b551662fb306cf8cad286db43d
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2012-12-26 19:58:25 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 ie6" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8 ie7" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9 ie8" lang="es"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10 ie9" lang="es"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+<head>
+	<title>Peugeot: Test Drive</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+	<!--[if lt IE 9]><script src="js/libs/html5.js"></script><![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="css/global.css" />
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<h1><a href="#"><span class="hide">Peugeot</span></a></h1>
+			<h2>Motion & Emotion</h2>
+		</div>
+	</header>
+
+
+	<section class="home">
+		<div class="container">
+			<h1>1. TEST DRIVE DE VERANO</h1>
+			<h2>SELECCIONE UBICACIÓN O MODELO</h2>
+
+			<p>Este verano te invitamos a descubrir nuestros más modernos modelos en distintos puntos estrategicamente ubicados en las costas de Carilo y Pinamar. Para reservar tu turno empeza eligiendo el modelo o ubicación que más interese.</p>
+
+			<p><a href="#">Por modelo</a></p>
+			<p><a href="#">Por ubicación</a></p>
+		</div>
+	</section>
+
+	<section class="ubicacion step2">
+		<div class="container">
+			<h1>2. SELECCIONA LA UBICACIÓN DESEADA</h1>
+		</div>
+	</section>
+
+	<section class="ubicacion step3">
+		<div class="container">
+			<h1>3. CONFIRMAR VEHICULO</h1>
+			<p>EN EL STAND PINAMAR EL LUNES 07/01/13 de 9 a 12:30 y de 16:30 a 20:00 Tenes DISPONIBLEs LOS SIGUIENTES VEHICULOS....</p>
+		</div>
+	</section>
+
+
+
+
+	<script src="js/libs/jquery.min.js"></script>
+
+</body>
+</html>
